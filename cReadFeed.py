11f3ed121988504463580067ea3e01702ba20f90
Lautaro <acc.limayyo@gmail.com>
2015-04-06 02:15:42 -0300

    V0.1

---
+++ cReadFeed.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+'''
+Libreria: LV-readFeed
+Version: 0.1
+Autor: Lautaro Linquiman
+Lincencia: Creative Commons
+'''
+import os
+import sys
+if not (2,7) == sys.version_info[0:2]:
+	print 'Vercion para python 2.7'
+	print 'Abortando ejecucion'
+	sys.exit()
+
+try:
+	import urllib2
+except ImportError:
+	print 'Debes instalar el modulo urllib2'
+	print 'pip install urllib2'
+	sys.exit()
+
+try:
+	from bs4 import BeautifulSoup
+except:
+	print 'Debes instalar el modulo beautifulsoup4'
+	print 'pip install beautifulsoup4'
+	sys.exit()
+
+class readFeed:
+	def __downloadFeed(self):
+		#Descarga el contenido del feed
+		try:
+			f = urllib2.urlopen(self.__url)
+			self.__sourceCode = f.read()
+			return 1
+		except:
+			return 0
+
+	def __getContentFeed(self):
+		#Traduce las variables del feed a listas
+		self.__source = BeautifulSoup(self.__sourceCode)
+
+		if(self.__type == 'rss'):
+			channel = self.__source.select('channel')[0]
+			self.__chanelInfo['title'] = channel.select('title')[0].get_text()
+			self.__chanelInfo['description'] = channel.select('description')[0].get_text()
+			self.__chanelInfo['link'] = channel.select('link')[0].get_text()
+			self.__chanelInfo['update'] = channel.select('lastBuildDate')[0].get_text() #Problema
+			self.__chanelInfo['pubDate'] = channel.select('pubDate')[0].get_text()
+		elif(self.__type == 'atom'):
+			self.__chanelInfo['title'] = self.__source.select('title')[0].get_text()
+			self.__chanelInfo['description'] = self.__source.select('subtitle')[0].get_text()
+			self.__chanelInfo['link'] = self.__source.select('link[rel="alternate"]')[0].attrs.get('href')
+			self.__chanelInfo['update'] = self.__source.select('updated')[0].get_text()
+			for entry in self.__source.select('entry'):
+				entryTitle = entry.select('title')[0].get_text()
+				entryLink = entry.select('link[rel="alternate"]')[0].attrs.get('href')
+				entryDic = {'title': entryTitle, 'link': entryLink}
+				self.__entries.append(entryDic)
+
+	def getSource(self):
+		return self.__sourceCode
+
+	def getChannelInfo(self):
+		return self.__chanelInfo
+
+	def getEntries(self):
+		return self.__entries
+
+	def getEntry(self, idEntry):
+		return self.__entries[idEntry]
+
+	def __add__(self, rF):
+		for entry in rF.getEntries():
+			self.__entries.append(entry)
+			
+	def __init__(self, url, typeFeed='rss'):
+		self.__url = url
+		self.__type = typeFeed
+		self.__source = 0
+		self.__sourceCode = ''
+		self.__chanelInfo = {'title': '', 'description': '', 'link': '', 'update': '', 'pubDate': ''}
+		self.__entries = []
+		if(not self.__downloadFeed()):
+			print 'Error al descargar el codigo'
+			print 'Aborando ejecucion'
+			sys.exit()
+		self.__getContentFeed()
+		
---
+++ example01.py
@@ -0,0 +1,32 @@
+#!usr/bin/python
+'''
+Libreria: LV-readFeed
+Version: 0.1
+Autor: Lautaro Linquiman
+Lincencia: Creative Commons
+'''
+from cReadFeed import readFeed
+def main():
+	feed = readFeed('http://www.tutorialdeprogramacion.com/feeds/posts/default', 'atom') 
+	''' Descargando y estructurando el Feed '''
+	feed.getSource()
+	''' Retorna el codigo fuente del Feed en texto plano'''
+	feed.getChannelInfo()
+	''' Retorna informacion del canal dic {title, descripcion, link, update} '''
+	feed.getEntries()
+	''' Retorna una lista con todas las entradas obtenidas del feed
+		list [0:] entrada ID
+			[0:] dic {title, link} '''
+	feed.getEntry(1)
+	''' Retorna la informacion de la entrada indicada 
+		dic {title, link} '''
+
+	#Sobre carga de la clase feed
+	print 'Cantidad de entradas antes de la sobre carga ' , len(feed.getEntries())
+
+	feed1 = readFeed('http://www.tutorialdeprogramacion.com/feeds/posts/default', 'atom') 
+
+	feed+feed1
+
+	print 'Cantidad de entradas despues de la sobre carga ' , len(feed.getEntries())
+main()
